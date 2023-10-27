c28fb6498858bf3e882fa04425b82a1d01113bbf
wuchenhaoo <38174336+wuchenhaoo@users.noreply.github.com>
2023-10-27 21:38:20 +0800

    Create image_datasets.py

---
+++ datasets/image_datasets.py
@@ -0,0 +1,15 @@
+train_globs = {
+        "data_0": "/path/to/train/folder/*.png",
+        "data_1": "/path/to/train/folder/*.png",
+        "data_2": "/path/to/train/folder/*.png",
+        "data_3": "/path/to/train/folder/*.png",
+        "data_4": "/path/to/train/folder/*.png"
+    }
+
+test_globs = {
+        "data_0": "/path/to/test/folder/*.png",
+        "data_1": "/path/to/test/folder/*.png",
+        "data_2": "/path/to/test/folder/*.png",
+        "data_3": "/path/to/test/folder/*.png",
+        "data_4": "/path/to/test/folder/*.png"
+    }
